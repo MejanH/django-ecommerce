ae4ee2008e74c2331ce8e7c7d827a199a62359f9
MejanH <muhammad.mejanul.haque@gmail.com>
2021-07-27 18:15:41 +0600

    unit testing

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # Ecommerce Web Application with Django
 
-description goes here...
+First Django Application after learning Python 😃. I am really enjoying Python and Django. Everything is same like Node.js except syntax difference and battery included in Django. It was faster than expressjs. Loved it.
+
+**Features implemented till now**:
+
+- Both Django Admin and Rest Framework at the same time from login and browsable api to Django Admin section
+- Working Authenticated Product and Category Models
+- Readonly products and categories
+- Postgresql
+
+I saw a library called [Graphene-Django](https://docs.graphene-python.org/projects/django/en/latest/) which is capable of adding graphql in django. Right now, I used Rest API architechture. So, Ignored it for now.
 
 ### Prequesties
 
+- Used `pipenv` instead of `pip` package manager. `pipenv` felt more like Node.js `package.json`, so went for it. also, I could see how many packages/modules I installed and run custom scripts with `[scripts]` section. So, definitely recommend it.
 - There are some default libraries need to be installed for django like "Pellow"
 
 ### Ecommerce Data Schema
--- backend/ecommerce/tests.py
+++ backend/ecommerce/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from .models import Category, Product
 
 # Create your tests here.
+
+
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Test Category')
+
+    def test_category_name(self):
+        """
+        Test the created Category name
+        """
+        self.assertEqual(self.category.name, 'Test Category')
+
+
+class ProductTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Test Category')
+        self.product = Product.objects.create(
+            name='Test Product', price=10.00, category=self.category)
+
+    def test_product_name(self):
+        """
+        Test the created Product name
+        """
+        self.assertEqual(self.product.name, 'Test Product')
+
+    def test_product_price(self):
+        """
+        Test the created Product price
+        """
+        self.assertEqual(self.product.price, 10.00)
+
+    def test_product_category(self):
+        """
+        Test the created Product category
+        """
+        self.assertEqual(self.product.category, self.category)
